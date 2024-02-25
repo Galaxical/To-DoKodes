b318901ecaa3af076cf403ef42705c8732b8e546
azisomtochukwu@gmail.com <azisomtochukwu@gmail.com>
2024-02-25 23:30:35 +0100

    function codes

--- To-Do.js
+++ To-Do.js
@@ -1,40 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(){
-    //access local storage
-    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-
-    displayTasks();
-})
-
-//create function to add task
-
-function addTask(){
-    const taskInput = document.getElementById("TodoInput");
-    const taskList = document.getElementById("TaskList");
+function addTask() {
+    const taskInput = document.getElementById("todoInput");
+    const taskList = document.getElementById("todoList");
     const taskText = taskInput.value;
 
-    if (taskText !== ""){
-        const li = document.createElement("li");
+      if (taskText !== "") {
+        const li = document.createElement("li")
         li.innerHTML = '<input type="text" value="' + taskText + '" readonly>' +
-        `<button onClick = "deleteTask(this)">Delete</button>`;
-        taskList.appendChild("li");
-        taskInput.value = ""; 
+        '<button onclick="deleteTask(this)">Delete</button>';
+        taskList.appendChild(li);
+        taskInput.value = "";
+      }
     }
-}
-
+    
+    function deleteTask(element) {
+        const taskList = document.getElementById("todoList");
+        const li = element.parentNode;
+        taskList.removeChild(li);
+    }
 //delete task element
 function deleteTask (){
     const taskList = document.getElementById("TodoList")
     const li = element.parentNode;
     taskList.removeChild(li)
 }
-//store and retrieve tasks from local storage
-const storeTasks = localStorage.getItem('tasks');
-if(storeTasks){
-    tasks = JSON.parse(storeTasks);
-    tasks.forEach(task=> addTask(task));
-}
-
-window.addEventListener('beforeunload', () => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-});
